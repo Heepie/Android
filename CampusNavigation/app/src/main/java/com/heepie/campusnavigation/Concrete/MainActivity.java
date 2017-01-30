376b68d6c53d942846a74ed1a37158143f4027fe
heepie <gheepie@gmail.com>
2017-01-30 23:08:49 +0900

    divide frame and concrete

--- CampusNavigation/app/src/main/java/com/heepie/campusnavigation/Concrete/MainActivity.java
+++ CampusNavigation/app/src/main/java/com/heepie/campusnavigation/Concrete/MainActivity.java
@@ -1,7 +1,9 @@
-package com.heepie.campusnavigation;
+package com.heepie.campusnavigation.Concrete;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.heepie.campusnavigation.R;
 
 public class MainActivity extends AppCompatActivity {
 
